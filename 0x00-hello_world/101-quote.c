b8a4baa28aba02c7f9c4b4eb747ba28611e12a99
dannycod3r <oseid7807@gmail.com>
2023-02-16 17:14:02 +0000

    Add code to print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Description: Prints to stderr
+ * Return: 1
+ */
+int main(void)
+{
+	fprintf(stderr,
+	    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
