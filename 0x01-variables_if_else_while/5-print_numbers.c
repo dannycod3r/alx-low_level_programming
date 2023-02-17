7fa922933fa59981223e319dbce4394dc0382f5e
dannycod3r <oseid7807@gmail.com>
2023-02-17 16:34:12 +0000

    Print single digit numbers of base 10 (0..9)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Description: program to print 0..9
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+	while (num <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	return (0);
+}
