cfdc871df55cfeb67bd095cb7188e441b3fd4b6c
dannycod3r <oseid7807@gmail.com>
2023-02-17 15:05:10 +0000

    Print alphabets a..z end with a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Description: something
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
