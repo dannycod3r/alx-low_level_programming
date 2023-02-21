31ac28eb62cd3dccdf449afd3a530ff3684584d9
dannycod3r <oseid7807@gmail.com>
2023-02-21 18:05:30 +0000

    print alphabet implemnt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ *
+ * Description:
+ * Return: 0
+ */
+
+int print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; a <= 'z'; i++ )
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
