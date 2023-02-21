f1df4754370f04a5241e20803c9c4244e60cee8b
dannycod3r <oseid7807@gmail.com>
2023-02-21 18:24:01 +0000

    print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - program entry
+ *
+ * Description:
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z', j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,4 +7,7 @@ int _putchar(int);
 /*print alphabets*/
 void print_alphabet(void);
 
+/* print alphabets 10x*/
+void print_alphabet_x10(void);
+
 #endif
