8816ece1de4ddfae73dafbc422c2291cfd4dca6d
dannycod3r <oseid7807@gmail.com>
2023-02-22 16:10:38 +0000

    add even fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  program entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1;
+	int b;
+	int c = 0;
+	int sum = 0;
+
+	for (b = 2; b < 4000000; b++)
+	{
+		if (b % 2 == 0)
+			sum = sum + b;
+		c = b;
+		b = b + a;
+		a = c
+	}
+	printf("%d\n", sum)
+	return (0);
+}
