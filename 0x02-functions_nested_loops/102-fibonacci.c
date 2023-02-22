72260555510565e6e400a8dd6fbd9bad3d1694ec
dannycod3r <oseid7807@gmail.com>
2023-02-22 15:05:42 +0000

    print fibbonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * main - program entry
+ *
+ * Description:
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i; /*looper */
+	long int a = 1; /*first term */
+	long int b = 2; /*second term */
+	long int c = a + b; /*third number */
+
+	printf("%lu, %lu, ", a, b);
+	for (i = 2; i <= 50; i++)
+	{
+		if (i != 50)
+		{
+			printf("%lu, ", c);
+		}
+		else
+		{
+			printf("%lu\n", c);
+		}
+
+		a = b;
+		b = c;
+		c = a + b;
+	}
+
+	return (0);
+}
