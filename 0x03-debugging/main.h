7de1e93f7e1681e34d1b87b3557b0ff08cfbca67
dannycod3r <oseid7807@gmail.com>
2023-02-22 23:39:29 +0000

    update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*#include <stdio.h>*/
+#include <stdio.h>
 
+/*positive or negative*/
 void positive_or_negative(int i);
-
 
 /*largest of three*/
 int largest_number(int, int, int);
+
+/*convert date*/
+int convert_day(int, int);
+
+/*print remaining days*/
+void print_remaining_days(int, int, int)
 
 #endif /* MAIN_H */
