41c3a462465a08a4c02db6e5c0d58ffa983f0fdd
dannycod3r <oseid7807@gmail.com>
2023-02-24 00:05:46 +0000

    update looper, to suite char type of _putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,9 +4,9 @@
  */
 void print_numbers(void)
 {
-	int i = '0'; /*looper, and first value*/
+	int i = 0; /*looper, and first value*/
 
-	while (i <= '9')
+	while (i <= 9)
 	{
 		_putchar(i);
 		i++;
