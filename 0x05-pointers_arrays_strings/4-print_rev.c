2c1f001cedaffa7220059284989bfcb9e5670952
dannycod3r <oseid7807@gmail.com>
2023-02-27 22:48:14 +0000

    fix betty, fix overcounting

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,11 @@ void print_rev(char *s)
 	/*get the legth of the string*/
 	int len = 0;
 
-	while(s[len] != '\0') /*get the length*/
+	while (s[len] != '\0') /*get the length*/
 		len++;
 
 	/*len--;*/ /*do this to ignore adding the '\0'*/
-	while(len >= 0)
+	while (len > 0)
 	{
 		_putchar(s[len]);
 		len--;
