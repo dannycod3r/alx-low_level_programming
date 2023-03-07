b190821b46542fbee7ac1af763c8ef44f69eebd7
dannycod3r <oseid7807@gmail.com>
2023-03-07 18:01:07 +0000

    return the first occurance of a letter, else null

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: pointer to a string
+ * @c: chareacter in search of
+ * Return: a pointer to the first occurrence of the character c in the string s
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /*check if char c is in s*/
+	{
+		if (s[i] == c) /*if found jump from the loop*/
+			break;
+		i++;
+	}
+
+	/*i = position, return pointer to s[i]*/
+	if (s[i])
+		return (s + i);
+	else
+		return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,8 +4,10 @@
 /*fill the memory with constant bytes*/
 char *_memset(char *s, char b, unsigned int n);
 
-
 /*copy memory area*/
 char *_memcpy(char *dest, char *src, unsigned int n);
+
+/*locate a character in a string*/
+char *_strchr(char *s, char c);
 
 #endif
