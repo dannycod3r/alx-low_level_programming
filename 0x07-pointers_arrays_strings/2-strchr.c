424dfe23f0169bb0fcd512379cef6d99bdf3b315
dannycod3r <oseid7807@gmail.com>
2023-03-07 18:44:48 +0000

    fix counting

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,12 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0') /*check if char c is in s*/
-	{
-		if (s[i] == c) /*if found jump from the loop*/
-			return (s + i);
-
+	while (s[i] != '\0' && s[i] != c) /*check if char c is in s*/
 		i++;
-	}
+
 
 	/*i = position, return pointer to s[i]*/
-	if (s[i] != '\0')
+	if (s[i] == c)
 		return (s + i);
 	else
 		return ('\0');
