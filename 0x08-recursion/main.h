a39ee981fc19e421e1df4350470ca487fb256c31
dannycod3r <oseid7807@gmail.com>
2023-03-08 14:53:29 +0000

    reverse string using recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /*a function that prints a string followed by new line*/
 void _puts_recursion(char *s);
 
+/*reverse a string using recursion*/
+void _print_rev_recursion(char *s);
+
 #endif
