0fbdcf052f09c2d8e2ef10d287a544a20fc2a0e1
dannycod3r <oseid7807@gmail.com>
2023-03-11 02:03:19 +0000

    Multiply in args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that returns the product of args passed to it
+ * @argc: arg count
+ * @argv: pointer to argument
+ * Return: number of args passed, 0 if none
+ */
+int main(int  argc, char *argv[])
+{
+	int i; /*iterator*/
+	int mult = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+			mult *= atoi(argv[i]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
