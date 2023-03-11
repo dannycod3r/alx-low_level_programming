4ba18f2e8b6ed2280a5f75c1c683e48d51d1bdaf
dannycod3r <oseid7807@gmail.com>
2023-03-11 01:28:48 +0000

    fix betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - program that returns the number of args passed to it
+ * @argc: arg count
+ * @argv: pointer to argument
+ * Return: number of args passed, 0 if none
+ */
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 	int i; /*iterator*/
 
 	if (argc > 1)
 	{
-		for (i=1; i < argc; i++)
+		for (i = 1; i < argc; i++)
 			;
 		printf("%d\n", --i);
 	}
