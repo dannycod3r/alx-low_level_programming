80143b48d497ab10c29100269c83edfaa777bb06
dannycod3r <oseid7807@gmail.com>
2023-03-16 00:28:24 +0000

    Divide string to words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,4 +13,9 @@ void free_grid(int **grid, int height);
 
 char *argstostr(int ac, char **av);
 
+char **strtow(char *str);
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
 #endif
