e3082c0350d054e9cba921f6192b2997897ab643
dannycod3r <oseid7807@gmail.com>
2023-03-20 18:58:12 +0000

    Create a dog structure with members, name, age, owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - create a dog type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
