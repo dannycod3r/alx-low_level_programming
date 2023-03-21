9f793c8b466b066909effa3b95eb41e5bfcb4698
dannycod3r <oseid7807@gmail.com>
2023-03-21 00:22:40 +0000

    typedef struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 /* initialize a variable of type struct dog*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
