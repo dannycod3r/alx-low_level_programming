1371a84f3bbc659276a3f76de5bdee3002dec8ff
dannycod3r <oseid7807@gmail.com>
2023-03-31 21:14:53 +0000

    Remove space

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 void print_first(void)
 {
 printf(
-"You're beat! and yet, you must allow, \nI bore my house upon my back!\n"
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
 );
 }
