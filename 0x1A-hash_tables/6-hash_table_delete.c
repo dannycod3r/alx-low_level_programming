3d44983d24631be8645f7666ebe2398f670c2444
dannycod3r <oseid7807@gmail.com>
2023-06-09 13:53:47 -0700

    Feat Add function to delete a hash table and free the memory

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table and free the memory
+ * @ht: the hash table
+ *Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
